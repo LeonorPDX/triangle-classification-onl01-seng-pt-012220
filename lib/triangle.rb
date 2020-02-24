9059f7c2779dab50ac73ff43cd49b9e70aed305b
Leonor Colbert <leonoracolbert@gmail.com>
2020-02-24 21:31:31 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,14 +1,17 @@
 class Triangle
   attr_accessor :s1, :s2, :s3
   
+  @sides = []
+  
   def initialize (side1, side2, side3)
     @s1 = side1
     @s2 = side2
     @s3 = side3
+    @sides << @s1, @s2, @s3
   end
   
   def kind
-    
+    if @sides.each {|side| side !> 0
   end
   
   class TriangleError < StandardError
